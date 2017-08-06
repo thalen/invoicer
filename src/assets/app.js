8af1f5728fb9458bf27475c77b11ef9b69257072
Olle Thalen <olle.thalen@omegapoint.se>
2017-08-06 13:21:03 +0200

    fix for printing

--- src/assets/app.js
+++ src/assets/app.js
@@ -1,7 +1,9 @@
 var fs = require('fs');
 var pdf = require('html-pdf');
 var html = fs.readFileSync('invoice.html', 'utf8');
-var options = { format: 'Letter' };
+var options = {
+  "format": "A4"
+};
 
 pdf.create(html, options).toFile('invoice.pdf', function(err, res) {
   if (err) return console.log(err);
