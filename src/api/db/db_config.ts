eca56a2137b5ca1c49c927d80c29c876db680490
Olle Thalén <olle@thalen-solutions.com>
2020-08-21 09:02:46 +0200

    updating connection string

--- src/api/db/db_config.ts
+++ src/api/db/db_config.ts
@@ -1,7 +1,7 @@
 export default {
-    env: process.env.NODE_ENV || 'development',
-    base_url: process.env.BASE_URL || 'http://localhost:3000',
-    db: {
-        uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/invoicer-api',
-    }
-}
+  env: process.env.NODE_ENV || 'development',
+  base_url: process.env.BASE_URL || 'http://localhost:3000',
+  db: {
+    uri: process.env.DB_URI || 'mongodb://127.0.0.1:27017/invoicer-api'
+  }
+};
